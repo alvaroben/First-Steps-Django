6a03229370d9f62bea3458cb066b64d17034c4a3
jeuel <jeuelortiz17@gmail.com>
2024-08-24 11:08:11 -0400

    terminado proyecto

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Car (models.Model):
+    model = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    car_type = models.CharField(max_length=50)
+    year = models.IntegerFieldField()
