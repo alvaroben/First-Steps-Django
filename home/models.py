5f4406908914af3d5f1454d5133289ac0368ab85
jeuel <jeuelortiz17@gmail.com>
2024-08-24 11:59:28 -0400

    terminadoproyecto

--- home/models.py
+++ home/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
 class Car (models.Model):
+    car_TYPE = (("jepeta", "Jepeta"),
+                ("camioneta", "Camioneta"),
+                ("deportivo", "Deportivo"),
+                ("seldan", "Selda"))
+
+
+
     model = models.CharField(max_length=50)
     brand = models.CharField(max_length=50)
-    car_type = models.CharField(max_length=50)
-    year = models.IntegerFieldField()
+    car_type = models.CharField(max_length=50, choices=car_TYPE)
+    year = models.IntegerField()
--- home/views.py
+++ home/views.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.views.generic import ListView
 
-from login.models import MyUser
+from home.models import Car
 
 
-@login_required
-def home_view(request):
-    user = MyUser.objects.last()
-    return render(request, "login/home.html", {"user": user})
+class HomeView(ListView):
+    modal = Car
