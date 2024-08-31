1cf1d57aaa43b7f936c07fa220e9030b4b60dfd3
Alvaro Bencosme <alvarobencosme@gmail.com>
2024-08-31 11:12:52 -0400

    add actions column

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from home.models import Car
+
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Car, CarAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    CAR_TYPES = (
+        ("jeepeta", "Jeepeta"),
+        ("camioneta", "Camioneta"),
+        ("deportivo", "Deportivo"),
+        ("sedan", "Sedán")
+    )
+
+    model = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    car_type = models.CharField(max_length=50, choices=CAR_TYPES)
+    year = models.IntegerField()
+
+    @property
+    def actions(self):
+        return """
+        <i style="color: blue;" class="bi bi-pencil"></i>
+        <i style="color: red" class="bi bi-trash"></i>
+        """
